d017763ca3236d556df20aed836138914447ae9b
ZhcChen <chen1406736605@gamil.com>
2026-07-16 15:00:24 +0800

    feat(wechat-miniprogram): 切换到原生小程序架构

---
+++ wechat-miniprogram/app.js
@@ -0,0 +1,10 @@
+App({
+  globalData: {
+    appName: 'Easy Teaching',
+    moduleName: 'wechat-miniprogram'
+  },
+
+  onLaunch() {
+    console.info('Easy Teaching WeChat Mini Program launched')
+  }
+})
---
+++ wechat-miniprogram/pages/home/index.js
@@ -0,0 +1,7 @@
+Page({
+  data: {
+    title: 'Easy Teaching',
+    subtitle: '原生微信小程序基础架构已初始化',
+    moduleName: 'wechat-miniprogram'
+  }
+})
